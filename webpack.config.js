dd68642cad6925632e60d488d65ae0713cda45d5
John Long <nakaroka@gmail.com>
2017-10-11 13:28:28 -0600

    Adds gzip

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const webpack = require('webpack');
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: './src/alleycat.js',
@@ -14,5 +16,14 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader'
     }]
-  }
+  },
+  plugins: [
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 0,
+      minRatio: 0.8
+    })
+  ]
 }
